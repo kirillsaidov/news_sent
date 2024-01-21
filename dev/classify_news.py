81ed56e3a3c5cc227c35906c920514b5f03df480
Kirill Saidov <kirillsaidovd@yahoo.com>
2024-01-21 16:17:10 +0600

    added news classification report"

---
+++ dev/classify_news.py
@@ -0,0 +1,34 @@
+# module classify_news
+
+import os
+import pandas as pd
+from datetime import datetime
+from collections import Counter
+from NewsSentiment import TargetSentimentClassifier
+
+# load data
+data_folder = './cache/webscrapping'
+df = pd.read_csv(os.path.join(data_folder, f'{datetime.now().strftime("%Y-%m-%d")}.csv'))
+
+# preprocess
+data = list(df.title.values)
+data = list(map(lambda x: ('', x, ''), data))
+
+# model
+tsc = TargetSentimentClassifier()
+
+# infer
+sentiments = tsc.infer(targets=data)
+
+# extract output labels names
+sentiments = list(map(lambda x: x[0]['class_label'], sentiments))
+counts = Counter(sentiments)
+
+# report
+print('------------------------------')
+print('REPORT: positive-neutral-negative')
+print(f' - [ {counts["positive"]:3d} | {counts["positive"]/len(sentiments):.2f}% ] positive')
+print(f' - [ {counts["neutral"]:3d} | {counts["neutral"]/len(sentiments):.2f}% ] neutral')
+print(f' - [ {counts["negative"]:3d} | {counts["negative"]/len(sentiments):.2f}% ] negative')
+print('------------------------------')
+
