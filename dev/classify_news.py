2180dc27ba5b8dc6d10df6c30d734960b67158fa
Kirill Saidov <kirillsaidovd@yahoo.com>
2024-01-23 21:55:17 +0600

    added news dump

--- dev/classify_news.py
+++ dev/classify_news.py
@@ -24,11 +24,21 @@ sentiments = tsc.infer(targets=data)
 sentiments = list(map(lambda x: x[0]['class_label'], sentiments))
 counts = Counter(sentiments)
 
+# append category to df
+df['Category'] = sentiments
+
 # report
 print('------------------------------')
 print('REPORT: positive-neutral-negative')
 print(f' - [ {counts["positive"]:3d} | {counts["positive"]/len(sentiments):.2f}% ] positive')
 print(f' - [ {counts["neutral"]:3d} | {counts["neutral"]/len(sentiments):.2f}% ] neutral')
 print(f' - [ {counts["negative"]:3d} | {counts["negative"]/len(sentiments):.2f}% ] negative')
+print('------------------------------')
+print('NEWS DUMP: negative')
+print(df.loc[df['Category'] == 'negative'])
+print('NEWS DUMP: positive')
+print(df.loc[df['Category'] == 'positive'])
+print('NEWS DUMP: neutral')
+print(df.loc[df['Category'] == 'neutral'])
 print('------------------------------')
 
