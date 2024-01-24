7d993c2eb110c3489190bc99279777d7d7ce7ed8
Kirill Saidov <kirillsaidovd@yahoo.com>
2024-01-24 21:38:58 +0600

    added more details to report; split into today, yesterday and all

--- dev/classify_news.py
+++ dev/classify_news.py
@@ -1,44 +1,73 @@
 # module classify_news
-
 import os
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 from collections import Counter
 from NewsSentiment import TargetSentimentClassifier
 
-# load data
-data_folder = './cache/webscrapping'
-df = pd.read_csv(os.path.join(data_folder, f'{datetime.now().strftime("%Y-%m-%d")}.csv'))
 
-# preprocess
-data = list(df.title.values)
-data = list(map(lambda x: ('', x, ''), data))
+def count_sentiments(df: pd.DataFrame, date: str = datetime.now().strftime("%Y-%m-%d")):
+    """Generate sentiments report
 
-# model
-tsc = TargetSentimentClassifier()
+    Args:
+        df (pd.DataFrame): dataframe with sentiments inferred
+        date (str, optional): date. If None, all data is used. Defaults to datetime.now().strftime("%Y-%m-%d").
 
-# infer
-sentiments = tsc.infer(targets=data)
+    Returns:
+        dict: counts of sentiments
+    """
+    dfd = df if date is None else df.loc[df['date'] == date]
+    sentiments = dfd['category'].values
+    counts = Counter(sentiments)
+        
+    # calculate
+    counts_ = {'date': date if date else 'ALL'}
+    for key, value in counts.items():
+        counts_.update({
+            key: (value, value/len(sentiments)),
+        })
+    
+    return counts_
 
-# extract output labels names
-sentiments = list(map(lambda x: x[0]['class_label'], sentiments))
-counts = Counter(sentiments)
 
-# append category to df
-df['Category'] = sentiments
+if __name__ == '__main__':
+    # load data
+    data_folder = './cache/webscrapping'
+    df = pd.read_csv(os.path.join(data_folder, f'{datetime.now().strftime("%Y-%m-%d")}.csv'))
+    # df.date = pd.to_datetime(df.date) # converts automatically upon loading
 
-# report
-print('------------------------------')
-print('REPORT: positive-neutral-negative')
-print(f' - [ {counts["positive"]:3d} | {counts["positive"]/len(sentiments):.2f}% ] positive')
-print(f' - [ {counts["neutral"]:3d} | {counts["neutral"]/len(sentiments):.2f}% ] neutral')
-print(f' - [ {counts["negative"]:3d} | {counts["negative"]/len(sentiments):.2f}% ] negative')
-print('------------------------------')
-print('NEWS DUMP: negative')
-print(df.loc[df['Category'] == 'negative'])
-print('NEWS DUMP: positive')
-print(df.loc[df['Category'] == 'positive'])
-print('NEWS DUMP: neutral')
-print(df.loc[df['Category'] == 'neutral'])
-print('------------------------------')
+    # preprocess
+    data = list(df.title.values)
+    data = list(map(lambda x: ('', x, ''), data))
+
+    # model
+    tsc = TargetSentimentClassifier()
+
+    # infer and extract label names
+    sentiments = tsc.infer(targets=data)
+    sentiments = list(map(lambda x: x[0]['class_label'], sentiments))
+    
+    # append category to df
+    df['category'] = sentiments
+
+    # count sentiments
+    counts = [
+        count_sentiments(df, date=datetime.now().strftime("%Y-%m-%d")),
+        count_sentiments(df, date=(datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")),
+        count_sentiments(df, date=None),
+    ]
+
+    # report
+    print('------------------------------')
+    for counts_ in counts:
+        print(f'REPORT: {counts_["date"]}')
+        for key, value in counts_.items():
+            if key == 'date': 
+                continue
+            print(f' - [ {value[0]:3d} | {value[1]:.2f}% ] {key}')                    
+    print('------------------------------')
+    for label in ['negative', 'positive', 'neutral']:
+        print(f'NEWS DUMP: {label}')
+        print(df.loc[df['category'] == label][['title', 'snippet', 'source', 'category', 'date']].sort_values(by='date', ascending=False).reset_index(drop=True), end='\n\n')
+    print('------------------------------')
 
